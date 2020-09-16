6274c56884542d830afd57ead678b86eddd0ad42
klick01 <klick01@email.franklin.edu>
2020-09-16 18:16:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
+/*
+ * Sarah Klick
+ * Webd325
+ * 9/16/2020
+ * Lab 1
+ *
+ *
+ * To run the project type `npm start` in the terminal
+ */
+
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let id = 0;
+
+const Todo = (props) => (
+  <div className="bottom-border-todo">
+    <li>
+      <input
+        className="checkbox"
+        type="checkbox"
+        checked={props.todo.checked}
+        onChange={props.onToggle}
+      />
+      <button className="button-delete" onClick={props.onDelete}>
+        Delete
+      </button>
+      <span>{props.todo.text}</span>
+    </li>
+  </div>
+);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
+
+  addTodo() {
+    const text = prompt('Enter your item!');
+    this.setState({
+      todos: [...this.state.todos, { id: id++, text: text, checked: false }],
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+    });
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id !== id) {
+          return todo;
+        } else {
+          return {
+            id: todo.id,
+            text: todo.text,
+            checked: !todo.checked,
+          };
+        }
+      }),
+    });
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div>
+          <h1>TODO App</h1>
+        </div>
+
+        <div>
+          <div>Total Items: {this.state.todos.length}</div>
+          <div>Remaining Items: {this.state.todos.filter((todo) => !todo.checked).length}</div>
+          <div className="bottom-border"></div>
+        </div>
+
+        <button onClick={() => this.addTodo()} className="button">
+          New TODO
+        </button>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <Todo
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
